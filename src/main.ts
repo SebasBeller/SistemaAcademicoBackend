0850dd78881866b31ed2b7290257579a8427bb12
Wama85 <wi_an9@hotmail.com>
2024-11-30 13:47:07 -0400

    se añade 2da url

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
     'http://35.164.95.156',
     'http://44.230.95.183',
     'http://44.229.200.200',
-    'https://academicofront.onrender.com/',
-    'http://localhost:4200' // Puedes dejar esto para pruebas locales
+    'https://academicofront.onrender.com',
+    'http://localhost:4200' 
   ];
 
   app.enableCors({
