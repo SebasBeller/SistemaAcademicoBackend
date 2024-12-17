163ae758b891b2bd9e26bdf6d9ba7b2c20d23ff8
Wama85 <wi_an9@hotmail.com>
2024-12-17 01:10:47 -0400

    se modifica regex en materia

--- src/materias/dto/create-materia.dto.ts
+++ src/materias/dto/create-materia.dto.ts
@@ -9,7 +9,7 @@ import {
 export class CreateMateriaDto {
     @IsString({ message: 'El nombre debe ser una cadena de texto' })
     @Length(5, 30, { message: 'El nombre de la materia debe tener entre 5 y 30 caracteres' })
-    @Matches(/^(?!\s*$)[a-zA-Z0-9\s]+$/, {
+    @Matches(/^(?!\s*$)[a-zA-Z0-9\sáéíóúüñÁÉÍÓÚÜÑ_-]+$/, {
         message: 'El nombre de la materia solo puede contener letras, números y espacios',
     })
     @IsNotEmpty({ message: 'El nombre no puede estar vacío' })
